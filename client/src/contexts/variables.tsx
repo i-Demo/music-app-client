24932cd0235393ba8b5ff5e6328c559aa971fd73
Hoai Vi <hoaivi.it@gmail.com>
2023-06-06 13:38:26 +0700

    Update Host

--- client/src/contexts/variables.tsx
+++ client/src/contexts/variables.tsx
@@ -1,6 +1,6 @@
 export const apiUrl =
     process.env.NODE_ENV !== "production"
-        ? "http://localhost:3000/api"
-        : "https://music-app-server-omega.vercel.app/api";
+        ? "http://localhost:5000/api"
+        : "https://music-app-r7c8.onrender.com//api";
 export const LOCAL_STORAGE_TOKEN_NAME = "music-app-auth";
 export const LOCAL_STORAGE_SONG_STATE_NAME = "music-app-song-state";
